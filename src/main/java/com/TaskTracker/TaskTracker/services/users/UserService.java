b53c0fb902fc274055221c54e8605dcf0624f4cb
seva <garanarsen@gmail.com>
2024-06-07 23:14:16 +0300

    feat: user service and controller created

---
+++ src/main/java/com/TaskTracker/TaskTracker/services/users/UserService.java
@@ -0,0 +1,18 @@
+package com.TaskTracker.TaskTracker.services.users;
+
+import com.TaskTracker.TaskTracker.DTO.users.UserRegistrationRequestDTO;
+import com.TaskTracker.TaskTracker.DTO.users.UserResponseDTO;
+
+import com.TaskTracker.TaskTracker.exceptions.teams.TeamNeverExistedException;
+import com.TaskTracker.TaskTracker.exceptions.teams.UserNotInTeamException;
+import com.TaskTracker.TaskTracker.exceptions.user.UserAlreadyExistsException;
+import com.TaskTracker.TaskTracker.exceptions.user.UserNeverExistedException;
+import com.TaskTracker.TaskTracker.models.User;
+
+import java.util.List;
+
+public interface UserService {
+    UserResponseDTO createUser(UserRegistrationRequestDTO userRegistrationRequestDTO) throws UserAlreadyExistsException;
+    UserResponseDTO userFindByEmail(String email) throws UserNeverExistedException;
+    List<UserResponseDTO> usersFindByTeam(String teamName) throws TeamNeverExistedException, UserNotInTeamException;
+}
